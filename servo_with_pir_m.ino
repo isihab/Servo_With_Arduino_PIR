e03cfe321c720c814820dbf4d95eb258ef207a6c
isihab <lifelesssihab@gmail.com>
2017-05-11 11:19:56 +0600

    Add files via upload

---
+++ servo_with_pir_m.ino
@@ -0,0 +1,32 @@
+/*
+ * Project Title: Servo Control With Arduino and PIR
+ * Author: M.S.U. Faisal
+ * Date  : 09.05.17
+ */
+ 
+
+#include <Servo.h>
+
+Servo miniservo; 
+int direc = 0;    
+
+void setup() {
+  miniservo.attach(7); 
+  pinMode(A0, INPUT);  
+}
+
+void loop() {
+  int pir_value = analogRead(A0);
+  if(pir_value>=675){
+    for (direc = 0; direc <= 90; direc += 1) { 
+    miniservo.write(direc);              
+    delay(10);          
+    }
+    delay(3000);
+    for (direc = 90; direc >= 0; direc -= 1) { 
+    miniservo.write(direc);             
+    delay(10); 
+    }
+ }
+}
+
